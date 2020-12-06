06df077e34994b69e80e9e0893d7c4f957261b87
JuanCamiloRengifo <juankacover@gmail.com>
2020-12-05 19:57:27 -0500

    Vesion 1.0.0 en desarrollo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-//import { FirebaseAppProvider}from 'reactfire';
-//import FirebaseConfig from './Firebase-config';
 //import 'firebase';
 ReactDOM.render(//(
-   // <FirebaseAppProvider firebaseConfig={FirebaseConfig}>
-   <App /> //</FirebaseAppProvider>
+   <App /> 
 , document.getElementById('root'));
