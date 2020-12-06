5d0f38b50cee8664fa72edc0c951517f3b599ef7
JuanCamiloRengifo <juankacover@gmail.com>
2020-12-05 19:25:52 -0500

    Vesion 1.0.0 en desarrollo

--- src/Firebase-config.js
+++ src/Firebase-config.js
@@ -1,1 +1,13 @@
-import fire from 'firebase';
+import firebase from 'firebase';
+
+var firebaseConfig = {
+   
+  // variables de configuracion de conexion a firebase
+};
+
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire; 
+
+
+
