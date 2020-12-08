c55a7de19a44bc720b4802e652ca55c0dead3e1c
JuanCamiloRengifo <juankacover@gmail.com>
2020-12-08 15:20:56 -0500

    Version 1.0.0 En Desarrollo

--- src/Firebase-config.js
+++ src/Firebase-config.js
@@ -3,14 +3,7 @@ import firebase from 'firebase';
 var firebaseConfig = {
    
   // variables de configuracion de conexion a firebase
-    apiKey: "AIzaSyC01nygfyue78XFgRsC6GAvKpSXraeLf_8",
-    authDomain: "csytion-cursos.firebaseapp.com",
-    databaseURL: "https://csytion-cursos.firebaseio.com",
-    projectId: "csytion-cursos",
-    storageBucket: "csytion-cursos.appspot.com",
-    messagingSenderId: "773100184307",
-    appId: "1:773100184307:web:8628442940f74343090d86",
-    measurementId: "G-LF0HD71QLK"
+  
 };
 
 const fire = firebase.initializeApp(firebaseConfig);
